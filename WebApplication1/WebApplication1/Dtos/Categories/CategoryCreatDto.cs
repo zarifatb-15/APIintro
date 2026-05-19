3f27ddc3514f12d9bd4803f89212744fea7642ba
zarifatb-15 <zarifatb@code.edu.az>
2026-05-19 12:00:00 +0400

    add scalar

--- WebApplication1/WebApplication1/Dtos/Categories/CategoryCreatDto.cs
+++ WebApplication1/WebApplication1/Dtos/Categories/CategoryCreatDto.cs
@@ -11,31 +11,26 @@ public class CategoryCreatDto
     public string Description { get; set; } =null!;
     // [FileTypes("image/jpeg", "image/png", "image/gif")]
     // [FileLength(5)]
-    public IFormFile Photo { get; set; }=null!;
+    public IFormFile? Photo { get; set; }
 }
 
 public class CategoryCreatDtoValidator : AbstractValidator<CategoryCreatDto>
 {
     public CategoryCreatDtoValidator()
     {
-        RuleFor(c => c.Name)
-            .NotEmpty()
-            .WithMessage("Name is required.")
-            .MaximumLength(100)
-            .WithMessage("Name cannot exceed 100 characters.");
-        
-        RuleFor(c => c.Description)
-            .NotEmpty()
-            .WithMessage("Description is required.")
-            .MaximumLength(500)
-            .WithMessage("Description cannot exceed 500 characters.");
-        
         RuleFor(c => c.Photo)
+            .Cascade(CascadeMode.Stop)
+
             .NotNull()
             .WithMessage("Photo is required.")
-            .Must(file => file.ContentType == "image/jpeg" || file.ContentType == "image/png" || file.ContentType == "image/gif")
+
+            .Must(file =>
+                file!.ContentType == "image/jpeg" ||
+                file.ContentType == "image/png" ||
+                file.ContentType == "image/gif")
             .WithMessage("Only JPEG, PNG, and GIF files are allowed.")
-            .Must(file => file.Length <= 2 * 1024 * 1024) // 5 MB
+
+            .Must(file => file!.Length <= 2 * 1024 * 1024)
             .WithMessage("File size must be less than or equal to 5 MB.");
     }
 }
--- WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981370c31f580fe13cbaafdd8b2d96680065cee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd66286a23529f97939ace9203fd0b440c8ffec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
