096c9588ddd12b9d01fd87ae81e4472fb3c3a13f
zarifatb-15 <zarifatb@code.edu.az>
2026-05-21 22:59:34 +0400

    add

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -1,15 +1,35 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Dtos.UserDtos;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class AccountController : ControllerBase
+public class AccountController
+    (
+        IValidator<RegisterDto> registerValidator,
+        UserManager<AppUser> userManager,
+        RoleManager<IdentityRole> roleManager
+    ) : ControllerBase
 {
     [HttpPost("register")]
 
-    public IActionResult Register()
+    public async Task <IActionResult> Register([FromBody] RegisterDto registerDto)
     {
+        var validationResult = registerValidator.Validate(registerDto);
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult.Errors);
+        var user = await userManager.FindByNameAsync(registerDto.UserName);
+        if(user is not null)
+            return BadRequest("Username already exists");
+        user = new AppUser()
+        {
+            FullName = registerDto.FullName,
+            UserName = registerDto.UserName,
+            Email = registerDto.Email
+        };
         return Ok();
     }
 }
---
+++ WebApplication1/WebApplication1/Dtos/UserDtos/RegisterDto.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+
+namespace WebApplication1.Dtos.UserDtos;
+
+public class RegisterDto
+{
+    public string FullName { get; set; } = null!;
+    public string UserName { get; set; }=null;
+    public string Password { get; set; }=null;
+    public string Email { get; set; }=null;
+    public string ConfirmPassword { get; set; }=null;
+}
+
+public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+{
+    public RegisterDtoValidator()
+    {
+        RuleFor(x => x.FullName).NotEmpty().WithMessage("FullName is required");
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Valid email is required");
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(6).WithMessage("Password is required");
+        RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage("Passwords are different");
+    }
+}
